5b8562c41354ee4dd90b4c8dd03ff9cab20a0757
GustavoBauny <gustavounipam1504@gmail.com>
2022-05-30 13:24:47 -0300

    Adicionando Desafio de código

---
+++ Projetos/DesafiosTQI/dragao.js
@@ -0,0 +1,17 @@
+
+let input = gets();
+
+let qtdEntradas = parseInt(input);
+let nivelEnergia;
+
+for(let i = 0; i < qtdEntradas; i++){
+    input = gets();
+    nivelEnergia = parseInt(input);
+    
+    if(  nivelEnergia <= 8000    ){
+       console.log('Inseto!');
+    }else{
+     console.log('Mais de 8000!');
+    }
+}
+
